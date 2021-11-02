085e5e118d42136841a56ff2f8f77951160604d1
Daniel Tisdall <daniel@informal.systems>
2021-11-02 17:45:34 +0000

    Adds a hello cli obj

--- mbt_python/mbt.py
+++ mbt_python/mbt.py
@@ -1,13 +1,28 @@
+import json
+
 import fire
 
 
+class Hello(object):
+    def explicit(self, foo, bar):
+        print(f"Hello {foo} {bar}")
+
+    def json(self, json_data_str):
+        print(f"{json_data_str=}")
+        json.loads(json_data_str)
+
+    def json_file(self, json_file_path):
+        print(f"{json_file_path=}")
+        pass
+
+
 class Modelator(object):
-    def apalache(self, name):
-        print(name)
+    def apalache(self):
+        raise Exception("noimpl")
 
     def tlc(self):
         raise Exception("noimpl")
 
 
 if __name__ == "__main__":
-    fire.Fire(Modelator)
+    fire.Fire({"hello": Hello})
