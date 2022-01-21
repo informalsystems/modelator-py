5adfa7249419199d7ce859920edcc8d14e326e92
Daniel <danwtisdall@gmail.com>
2022-01-21 13:33:25 +0000

    Impl TLC stdout extract_traces

--- modelator/util/parse/tlc_stdout.py
+++ modelator/util/parse/tlc_stdout.py
@@ -1,7 +1,19 @@
 def extract_traces(stdout: str):
     """
     Extract zero, one or more traces from the stdout of TLC.
-
-    TLC stdout is
     """
-    pass
+    ret = []
+    lines = stdout.split("\n")
+    HEADER = "State 1"
+    FOOTER = "Model checking completed."
+    header_cnt = 0
+    header_ix = -1
+    for i, line in enumerate(lines):
+        if line.startswith(HEADER) or line.startswith(FOOTER):
+            if 0 < header_cnt:
+                trace_lines = lines[header_ix:i]
+                trace = "\n".join(trace_lines)
+                ret.append(trace)
+            header_cnt += 1
+            header_ix = i
+    return ret
--- tests/util/parse/test_all.py
+++ tests/util/parse/test_all.py
@@ -1,5 +1,17 @@
+from ...helper import get_resource_dir
+from modelator.util.parse.tlc_stdout import extract_traces
+import os
+
+
 def test_tlc_stdout_extract_traces():
-    """
-    TODO:
-    """
+
+    fn = "TlcMultipleTraceParse.txt"
+    fn = os.path.join(get_resource_dir(), fn)
+    content = None
+    with open(fn, "r") as fd:
+        content = fd.read()
+
+    result = extract_traces(content)
+    assert len(result) == 4
+
     pass
