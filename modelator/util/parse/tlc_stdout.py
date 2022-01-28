438048805fd8b9a379f6840ef77c9e7730e05525
Daniel <danwtisdall@gmail.com>
2022-01-28 12:53:31 +0000

    Tidies up tlc_stdout.py

--- modelator/util/parse/tlc_stdout.py
+++ modelator/util/parse/tlc_stdout.py
@@ -4,39 +4,34 @@ def extract_traces(stdout: str):
 
     Note: Does not support lasso traces
     """
-    ret = []
-    lines = stdout.split("\n")
-    HEADER = "Error: Invariant"
-    FOOTER = "Model checking completed."
-    header_cnt = 0
-    header_ix = -1
-    for i, line in enumerate(lines):
-        if line.startswith(HEADER) or line.startswith(FOOTER):
-            if 0 < header_cnt:
-                trace_lines = lines[header_ix + 2 : i]
-                trace = "\n".join(trace_lines)
-                ret.append(trace)
-            header_cnt += 1
-            header_ix = i
-    return ret
 
+    def extract_trace_strings():
+        ret = []
+        lines = stdout.split("\n")
+        HEADER = "Error: Invariant"
+        FOOTER = "Model checking completed."
+        header_cnt = 0
+        header_ix = -1
+        for i, line in enumerate(lines):
+            if line.startswith(HEADER) or line.startswith(FOOTER):
+                if 0 < header_cnt:
+                    trace_lines = lines[header_ix + 2 : i]
+                    trace = "\n".join(trace_lines)
+                    ret.append(trace)
+                header_cnt += 1
+                header_ix = i
+        return ret
 
-def tla_trace_to_informal_trace_format_trace(trace):
-    """
-    Convert a tla trace from TLC stdout to the Informal Trace Format
-    https://apalache.informal.systems/docs/adr/015adr-trace.html?highlight=trace%20format#adr-015-informal-trace-format-in-json
-    """
-
-    def split_into_states(trace) -> list[str]:
+    def split_into_states(trace_str):
         """
         Returns a list of states.
 
-        A trace from TLC is a sequence of [header, content] pairs.
+        A trace string from TLC is a sequence of [header, content] pairs.
         The headers are not valid TLA+.
-        This function returns a list where each item is valid TLA+ content.
+        This function returns a list where each item is a TLA+ expression.
         """
         ret = []
-        lines = trace.split("\n")
+        lines = trace_str.split("\n")
         HEADER = "State "
         header_cnt = 0
         header_ix = -1
@@ -52,8 +47,6 @@ def tla_trace_to_informal_trace_format_trace(trace):
         ret = ["\n".join(lines) for lines in ret]
         return ret
 
-    trace = split_into_states(trace)
-    """
-    TODO: this is incomplete and is dependent on functionality to convert
-    TLA+ expressions to Informal Trace Format
-    """
+    trace_strings = extract_trace_strings()
+    traces = [split_into_states(s) for s in trace_strings]
+    return traces
