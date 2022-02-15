f4ec3332bb1c0097bea3339f0a1f55780b4de0ec
Daniel <danwtisdall@gmail.com>
2022-02-15 19:35:30 +0000

    Tweaks staking files

--- modelator/staking1.py
+++ modelator/staking1.py
@@ -3,6 +3,20 @@ import os
 from modelator.tlc.pure import tlc_pure
 from modelator.util.tlc.cli import tlc_itf
 import json
+from modelator.util.tlc.stdout_to_informal_trace_format import (
+    split_into_states,
+    extract_traces,
+)
+from modelator.util.tlc.state_to_informal_trace_format import (
+    state_to_informal_trace_format_state,
+)
+from modelator.util.informal_trace_format import (
+    ITFTrace,
+    JsonSerializer,
+    with_lists,
+    with_records,
+)
+from .staking1_aux import select_subset
 
 
 def fn(s):
@@ -12,6 +26,55 @@ def fn(s):
 
 def main():
 
-    # get TLC stdout
-    # split into trace strings
-    # run genetic algorithm to split them
+    txt = None
+    print("Reading TLC stdout")
+    with open(fn("tlc.7steps.action.outcome.out"), "r") as fd:
+        txt = fd.read()
+    print("Extracting trace strings")
+    traces = extract_traces(txt)
+    print(f"Processing {len(traces)} traces")
+    traces = [split_into_states(t) for t in traces]
+
+    def project(t):
+        s = set()
+        for i, state in enumerate(t):
+            outcome = None
+            nature = None
+            for line in state.split("\n"):
+                if "nature" in line:
+                    nature = line[line.find(">") + 3 : -2]
+                if "outcome" in line:
+                    outcome = line[line.find("=") + 3 : -1]
+            e = (i / 2, nature, outcome)
+            s.add(e)
+        return s
+
+    print(f"Projecting")
+    projected = [project(t) for t in traces]
+    print(f"Selecting subset")
+    indexes_of_best, loss_value, random_choice_loss_value = select_subset(
+        projected, target_size=64
+    )
+    traces = [traces[i] for i in indexes_of_best]
+    print(f"Subset selected")
+    print(f"Final loss value: {loss_value}")
+    print(f"Random loss value: {random_choice_loss_value}")
+    print(f"Ratio of random: {loss_value/random_choice_loss_value}")
+
+    def to_itf(t):
+        states = [state_to_informal_trace_format_state(state) for state in t]
+        vars = []
+        if 0 < len(states):
+            vars = list(states[0].var_value_map.keys())
+
+        return ITFTrace(vars, states)
+
+    print(f"Translating to ITF")
+    traces = [to_itf(t) for t in traces]
+    traces = [with_lists(t) for t in traces]
+    traces = [with_records(t) for t in traces]
+    print(f"Serializing")
+    traces = [JsonSerializer().visit(t) for t in traces]
+    with open(fn("model_based_testing_traces_auto.json"), "w") as fd:
+        fd.write(json.dumps(traces, indent=2))
+    print("Done.")
--- modelator/staking1_aux.py
+++ modelator/staking1_aux.py
@@ -1,4 +1,3 @@
-import json
 import random
 
 """
@@ -34,54 +33,13 @@ def pprint(s):
 PARAM_population_size = 50
 PARAM_target_size = 64
 PARAM_crossover_probability = 0.75
-PARAM_iterations = 100000
+PARAM_iterations = 160000
+PARAM_info_interval = 16000
 
 
-class Similarity:
-    def __init__(self, list_of_sets):
-        n = len(list_of_sets)
-        self.n = n
-        self.store = [0] * ((n * (n - 1)) // 2)
-        self.compute(list_of_sets)
+def select_subset(list_of_sets, target_size=PARAM_target_size):
 
-    def index(self, i, j):
-        ix = i * self.n + j
-        ix -= (i * (i + 1)) // 2
-        ix -= i + 1
-        return ix
-
-    def query(self, i, j):
-        if i == j:
-            return 1
-        if j < i:
-            i, j = j, i
-        ix = self.index(i, j)
-        return self.store[ix]
-
-    def compute(self, list_of_sets):
-        def similarity(A, B):
-            intersect = A.intersection(B)
-            union = A.union(B)
-            return round(len(intersect) / len(union), 3)
-
-        for i in range(0, self.n - 1):
-            for j in range(i + 1, self.n):
-                ix = self.index(i, j)
-                self.store[ix] = similarity(list_of_sets[i], list_of_sets[j])
-
-
-def select_subset(traces, project):
-
-    N = len(traces)
-    pprint("Starting")
-    projected = [project(t) for t in traces]
-    pprint("Finished projecting")
-    # similarity = Similarity(projected)
-    # pprint("Finished computing similarities")
-    # lowest_similarity = sorted(similarity.store)
-    # pprint(f"Smallest similarity: {lowest_similarity[0]}")
-    # lower_bound = (PARAM_target_size * PARAM_target_size * lowest_similarity[0]) / 2
-    # pprint(f"Lower bound on loss: {lower_bound}")
+    N = len(list_of_sets)
 
     def crossover(vx, vy):
         vx, vy = list(vx), list(vy)
@@ -113,27 +71,26 @@ def select_subset(traces, project):
         x = 0
         for i in range(len(v) - 1):
             for j in range(i + 1, len(v)):
-                # x += similarity.query(v[i], v[j])
-                x += similarity_f(projected[v[i]], projected[v[j]])
+                x += similarity_f(list_of_sets[v[i]], list_of_sets[v[j]])
         return x
 
     # Initialize population
     G = [None] * PARAM_population_size
     G_ixs = list(range(len(G)))
     for i in G_ixs:
-        G[i] = random.sample(range(N), PARAM_target_size)
+        G[i] = random.sample(range(N), target_size)
 
     def best():
         best_ix = 0
-        best_value = loss(G[0])
+        best_loss = loss(G[0])
         for i in range(1, len(G)):
             value = loss(G[i])
-            if value < best_value:
+            if value < best_loss:
                 best_ix = i
-                best_value = value
-        return G[best_ix], best_value
+                best_loss = value
+        return G[best_ix], best_loss
 
-    _, random_choice_value = best()
+    _, random_choice_loss = best()
 
     for k in range(PARAM_iterations):
         sxi, syi = random.sample(G_ixs, 2)
@@ -147,88 +104,9 @@ def select_subset(traces, project):
             if min(loss(sx), loss(sy)) <= min(loss(G[sxi]), loss(G[syi])):
                 G[sxi] = sx
                 G[syi] = sy
-        if k % 8000 == 0:
+        if k % PARAM_info_interval == 0:
             _, best_value = best()
-            pprint(best_value)
+            pprint(f"loss: {best_value}")
 
-    best, value = best()
-    best_traces = [traces[i] for i in best]
-    pprint(f"Loss with random choice                 : {random_choice_value}")
-    pprint(f"Loss after genetic selection            : {value}")
-    # pprint(f"Ratio worse than theoretical lower bound: {value/lower_bound}")
-    pprint(f"Ratio better than random choice         : {value/random_choice_value}")
-    return best_traces
-
-
-def project_to_action_outcome_set(trace):
-    def itf(trace):
-        s = set()
-        for state in trace["states"]:
-            nature = state["action"]["nature"]
-            outcome = state["outcome"]
-            redQsize = len(state["redelegationQ"]["#set"])
-            undQsize = len(state["undelegationQ"]["#set"])
-            valQsize = len(state["validatorQ"]["#set"])
-            # s.add((nature, outcome, redQsize, undQsize, valQsize))
-            s.add((nature, outcome))
-        return s
-
-    def raw_tla(trace):
-        s = set()
-        for state in trace:
-            outcome = None
-            nature = None
-            for line in state.split("\n"):
-                if "nature" in line:
-                    nature = line[line.find(">") :]
-                if "outcome" in line:
-                    outcome = line[line.find("=") :]
-            s.add((nature, outcome))
-        return s
-
-    return raw_tla(trace)
-
-
-def real_traces():
-    fn = "tlc.5steps.state_split.json"
-    traces = None
-    with open(fn, "r") as fd:
-        content = fd.read()
-        traces = json.loads(content)
-        # traces = json.loads(content)["traces"]
-    assert traces is not None
-    return traces
-
-
-def debug_traces():
-    fn = "debug.json"
-    traces = None
-    with open(fn, "r") as fd:
-        content = fd.read()
-        traces = json.loads(content)["traces"]
-    assert traces is not None
-    return traces
-
-
-def run():
-    pprint("Begin run()")
-
-    traces = real_traces()
-
-    pprint(f"Received {len(traces)} traces as input.")
-    # LIM = 40000
-    # if LIM < len(traces):
-    # pprint("WARNING: you have many traces which may harm performance, cropping")
-    # traces = random.sample(traces, LIM)
-
-    result = select_subset(traces, project_to_action_outcome_set)
-    fn = "tlc.5steps.state_split.{PARAM_target_size}.json"
-    with open(fn, "w") as fd:
-        obj = {}
-        obj["traces"] = result
-        s = json.dumps(obj, indent=4)
-        fd.write(s)
-
-
-if __name__ == "__main__":
-    run()
+    indexes_of_best_sets, loss_value = best()
+    return indexes_of_best_sets, loss_value, random_choice_loss
