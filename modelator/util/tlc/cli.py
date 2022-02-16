a52d66eb51673ae82363a60bec8a80ab5a0e7db8
Daniel <danwtisdall@gmail.com>
2022-02-16 11:33:10 +0000

    Refactors

--- modelator/util/tlc/cli.py
+++ modelator/util/tlc/cli.py
@@ -1,53 +1,5 @@
 import json as stdjson
-
-from recordclass import recordclass
-
-from modelator.helper import parallel_map
-
-from ..informal_trace_format import JsonSerializer, with_lists, with_records
-from .stdout_to_informal_trace_format import (
-    extract_traces,
-    tlc_trace_to_informal_trace_format_trace,
-)
-
-# mypy: ignore-errors
-
-cmd_fields = (
-    "stdout",  # Captured stdout from TLC execution
-    "lists",  # Transform 1-indexed TLA+ functions into lists
-    "records",  # Transform string indexed functions into records
-)
-
-Cmd = recordclass("Cmd", cmd_fields, defaults=(None,) * len(cmd_fields))
-
-
-def json_to_cmd(json) -> Cmd:
-    json = {"stdout": None, "lists": True, "records": True} | json
-    cmd = Cmd()
-    cmd.stdout = json["stdout"]
-    cmd.lists = json["lists"]
-    cmd.records = json["records"]
-    return cmd
-
-
-def tlc_itf(*, cmd=None, json=None):  # types: ignore
-    """
-    Extract a list of execution traces in the Informal Trace Format
-    from the stdout of a TLC execution.
-    """
-
-    if json is not None:
-        cmd = json_to_cmd(json)
-
-    assert cmd.stdout is not None, "tlc_itf requires TLC's stdout as input data"
-    tlc_traces = extract_traces(cmd.stdout)
-    itf_traces = parallel_map(tlc_trace_to_informal_trace_format_trace, tlc_traces)
-    if cmd.lists:
-        itf_traces = parallel_map(with_lists, itf_traces)
-    if cmd.records:
-        itf_traces = parallel_map(with_records, itf_traces)
-    itf_traces_objects = parallel_map(lambda t: JsonSerializer().visit(t), itf_traces)
-    return itf_traces_objects
+from .itf import tlc_itf, TLCITFCmd
 
 
 class Tlc:
@@ -69,7 +21,7 @@ class Tlc:
             assert (
                 self._stdin is not None
             ), "TLC's stdout string should be passed on stdin if not passing json"
-            cmd = Cmd()
+            cmd = TLCITFCmd()
             cmd.stdout = self._stdin.read()
             cmd.lists = lists
             cmd.records = records
---
+++ modelator/util/tlc/itf.py
@@ -0,0 +1,50 @@
+import json as stdjson
+
+from recordclass import recordclass
+
+from modelator.helper import parallel_map
+
+from ..informal_trace_format import JsonSerializer, with_lists, with_records
+from .stdout_to_informal_trace_format import (
+    extract_traces,
+    tlc_trace_to_informal_trace_format_trace,
+)
+
+# mypy: ignore-errors
+
+cmd_fields = (
+    "stdout",  # Captured stdout from TLC execution
+    "lists",  # Transform 1-indexed TLA+ functions into lists
+    "records",  # Transform string indexed functions into records
+)
+
+TLCITFCmd = recordclass("Cmd", cmd_fields, defaults=(None,) * len(cmd_fields))
+
+
+def json_to_cmd(json) -> TLCITFCmd:
+    json = {"stdout": None, "lists": True, "records": True} | json
+    cmd = TLCITFCmd()
+    cmd.stdout = json["stdout"]
+    cmd.lists = json["lists"]
+    cmd.records = json["records"]
+    return cmd
+
+
+def tlc_itf(*, cmd=None, json=None):  # types: ignore
+    """
+    Extract a list of execution traces in the Informal Trace Format
+    from the stdout of a TLC execution.
+    """
+
+    if json is not None:
+        cmd = json_to_cmd(json)
+
+    assert cmd.stdout is not None, "tlc_itf requires TLC's stdout as input data"
+    tlc_traces = extract_traces(cmd.stdout)
+    itf_traces = parallel_map(tlc_trace_to_informal_trace_format_trace, tlc_traces)
+    if cmd.lists:
+        itf_traces = parallel_map(with_lists, itf_traces)
+    if cmd.records:
+        itf_traces = parallel_map(with_records, itf_traces)
+    itf_traces_objects = parallel_map(lambda t: JsonSerializer().visit(t), itf_traces)
+    return itf_traces_objects
