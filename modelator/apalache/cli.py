e6631441cd5cdde78adf6b8ebaea2a40ff57acd7
Daniel <danwtisdall@gmail.com>
2022-03-30 09:16:55 +0100

    Updates Apalache cli class

--- modelator/apalache/cli.py
+++ modelator/apalache/cli.py
@@ -1,107 +1,83 @@
-import json
+import json as stdjson
 
 from .pure import apalache_pure
 from .raw import ApalacheArgs, RawCmd, apalache_raw
 
 
 class Apalache:
-    def __init__(self, stdin):
-        self.stdin = stdin
+    def __init__(self, stdin, stdout):
+        self._stdin = stdin
+        self._stdout = stdout
 
     def pure(self):
-        assert self.stdin is not None
-        data = json.loads(self.stdin.read())
-        result = apalache_pure(json_obj=data)
-        print(result)
+        """
+        Run Apalache without side effects using json input data.
+
+        Runs Apalache in a temporary directory, writing all necessary data into the
+        directory before calling Apalache, and reading back all necessary results
+        back into memory.
+
+        Writes the result to stdout in json.
+
+        Requires json input data on stdin (`<command> < data.json`).
+        """
+        assert (
+            self._stdin is not None
+        ), "The pure interface requires json input in stdin"
+        json_dict = stdjson.loads(self._stdin.read())
+
+        result = apalache_pure(json=json_dict)
+        to_print = stdjson.dumps(result, indent=4, sort_keys=True)
+        print(to_print, file=self._stdout)
 
     def raw(
         self,
         *,
-        stdin=None,
+        json=None,
         mem=False,
         cleanup=False,
         cwd=None,
         jar=None,
-        cmd=None,
-        file=None,
-        debug=None,
-        out_dir=None,
-        profiling=None,
-        smtprof=None,
-        write_intermediate=None,
-        algo=None,
-        cinit=None,
-        config=None,
-        discard_disabled=None,
-        init=None,
-        inv=None,
-        length=None,
-        max_error=None,
-        next=None,
-        no_deadlock=None,
-        nworkers=None,
-        smt_encoding=None,
-        tuning=None,
-        tuning_options=None,
-        view=None,
-        enable_stats=None,
-        output=None,
-        before=None,
-        action=None,
-        assertion=None,
-        infer_poly=None,
     ):
+        """
+        Run Apalache without removing side effects (for debugging).
+
+        Run Apalache directly without creating a temporary directory. This is mainly
+        useful for debugging. Arguments can be provided on command line or by
+        specifying the --json flag and providing json on stdin (`<command> < data.json`).
+
+        Arguments:
+            json : Read arguments from json instead of cli?
+            mem : TODO:
+            cleanup : TODO:
+            cwd : Full path to directory to run Apalache from.
+            jar : Full path to Apalache version 0.23.0 jar (other versions may work).
+            TODO:
+
+        """
         result = None
-        if stdin:
-            data = json.loads(self.stdin.read())
-            result = apalache_raw(json_obj=data)
+        if json:
+            json_dict = stdjson.loads(self.stdin.read())
+            result = apalache_raw(json_obj=json_dict)
         else:
             raw_cmd = RawCmd()
             raw_cmd.mem = mem
             raw_cmd.cleanup = cleanup
             raw_cmd.cwd = cwd
             raw_cmd.jar = jar
-            raw_cmd.args = ApalacheArgs(
-                cmd,
-                file,
-                debug,
-                out_dir,
-                profiling,
-                smtprof,
-                write_intermediate,
-                algo,
-                cinit,
-                config,
-                discard_disabled,
-                init,
-                inv,
-                length,
-                max_error,
-                next,
-                no_deadlock,
-                nworkers,
-                smt_encoding,
-                tuning,
-                tuning_options,
-                view,
-                enable_stats,
-                output,
-                before,
-                action,
-                assertion,
-                infer_poly,
-            )
+            raw_cmd.args = ApalacheArgs()
 
             result = apalache_raw(cmd=raw_cmd)
 
         stdout_pretty = result.process.stdout.decode("unicode_escape")
         stderr_pretty = result.process.stderr.decode("unicode_escape")
 
-        print(
-            f"""Ran 'apalache raw'.
-shell cmd used: {result.process.args}
-subprocess return code: {result.process.returncode}
-apalache output files: {result.files}
-stdout: {stdout_pretty}
-stderr: {stderr_pretty}"""
-        )
+        obj_to_print = {}
+        obj_to_print["shell_cmd"] = result.process.args
+        obj_to_print["return_code"] = result.process.returncode
+        obj_to_print["stdout"] = stdout_pretty
+        obj_to_print["stderr"] = stderr_pretty
+        obj_to_print["apalache_output_files"] = result.files
+
+        to_print = stdjson.dumps(obj_to_print, indent=4, sort_keys=True)
+        print(to_print, file=self._stdout)
