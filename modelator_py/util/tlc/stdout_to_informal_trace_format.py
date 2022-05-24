informalsystems/modelator-py

commit 848a08366d1b0a79a46ba049840b45d99923bf2c

init state violation with continue

modelator_py/util/tlc/stdout_to_informal_trace_format.py
@@ -25,7 +25,13 @@ def trace_lines_model_checking_mode(stdout) -> typing.List[typing.List[str]]:
 
     def is_start_of_new_trace(line):
         """When there are multiple traces, closes the previous trace"""
-        return line.startswith("Error: Invariant")
+
+        # when there are multiple violations, a new trace report starts with:
+        continue_case = line.startswith("Error: Invariant")
+
+        # when the first violation was in the init state, the second one starts with:
+        init_state_continue_case = line.startswith("Finished computing initial states")
+        return continue_case or init_state_continue_case
 
     def is_footer(line):
         multi_state_footer = (
@@ -39,14 +45,6 @@ def trace_lines_model_checking_mode(stdout) -> typing.List[typing.List[str]]:
 
         return single_state_footer or multi_state_footer
 
-    def format_single_state_trace(trace):
-        # we need this because for single-state trace errors ther will be no states
-        # enumerated in the output (but we need them in postprocessing)
-        if "State" not in trace[0]:
-            return ["State 1"] + trace
-        else:
-            return trace
-
     header_cnt = 0
     header_ix = -1
     for i, line in enumerate(lines):
@@ -54,7 +52,6 @@ def trace_lines_model_checking_mode(stdout) -> typing.List[typing.List[str]]:
             header_open = True
             if 0 < header_cnt:
                 trace = lines[header_ix + 1 : i]
-                trace = format_single_state_trace(trace)
                 ret.append(trace)
 
         if is_header(line):
@@ -67,7 +64,6 @@ def trace_lines_model_checking_mode(stdout) -> typing.List[typing.List[str]]:
             header_open = False
             if 0 < header_cnt:
                 trace = lines[header_ix + 1 : i]
-                trace = format_single_state_trace(trace)
                 ret.append(trace)
             break
 
@@ -124,7 +120,7 @@ def split_into_states(lines: typing.List[str]) -> typing.List[typing.List[str]]:
     header_cnt = 0
     header_ix = -1
     for i, line in enumerate(lines):
-        if line.startswith(HEADER):
+        if i == 0 or line.startswith(HEADER):
             if 0 < header_cnt:
                 ret.append(lines[header_ix + 1 : i])
             header_ix = i

tests/util/tlc/stdout_to_informal_trace_format_test.py
@@ -21,26 +21,32 @@ def test_extract_no_trace_from_tlc():
     assert len(tlc_traces) == 0
 
 
+def _extract_trace_from_tlc(fn):
+    fn = os.path.join(get_resource_dir(), fn)
+    content = None
+    with open(fn, "r") as fd:
+        content = fd.read()
+
+    tlc_traces = extract_traces(content)
+    return tlc_traces
+
+
 def test_extract_trace_from_tlc():
     fn = "TlcTraceParse.txt"
-    fn = os.path.join(get_resource_dir(), fn)
-    content = None
-    with open(fn, "r") as fd:
-        content = fd.read()
-
-    tlc_traces = extract_traces(content)
+    tlc_traces = _extract_trace_from_tlc(fn)
     assert len(tlc_traces) == 1
 
 
-def test_extract_trace_length1_from_tlc():
+def test_extract_trace_initState_from_tlc():
     fn = "TlcTraceParseInitState.txt"
-    fn = os.path.join(get_resource_dir(), fn)
-    content = None
-    with open(fn, "r") as fd:
-        content = fd.read()
-
-    tlc_traces = extract_traces(content)
+    tlc_traces = _extract_trace_from_tlc(fn)
     assert len(tlc_traces) == 1
+
+
+def test_extract_trace_initStateContinue_from_tlc():
+    fn = "TlcTraceParseInitStateContinue.txt"
+    tlc_traces = _extract_trace_from_tlc(fn)
+    assert len(tlc_traces) == 3
 
 
 def test_extract_trace_from_tlc_simulation_mode():

commit 62e8f3bdf5a8060f6b85c2bba28799c187b57ce8

fix for error introduced by 848a083

modelator_py/util/tlc/stdout_to_informal_trace_format.py
@@ -119,8 +119,13 @@ def split_into_states(lines: typing.List[str]) -> typing.List[typing.List[str]]:
     HEADER = "State "
     header_cnt = 0
     header_ix = -1
+
+    # this is for the case when the invariant is violated in the initial state
+    # then, the counterexample is not prefixed with "State "
+    if len(lines) > 0 and not lines[0].startswith(HEADER):
+        lines = [HEADER] + lines
     for i, line in enumerate(lines):
-        if i == 0 or line.startswith(HEADER):
+        if line.startswith(HEADER):
             if 0 < header_cnt:
                 ret.append(lines[header_ix + 1 : i])
             header_ix = i